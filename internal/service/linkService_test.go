e6fad82adb9db8b822f9cbfcdafa036ac73e7b8a
angelitl <angelitl@student.21-school.ru>
2026-03-07 23:00:50 +0300

    test: add link service tests and reform code

---
+++ internal/service/linkService_test.go
@@ -0,0 +1,55 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/Vadich007/shortener/internal/repository"
+	"github.com/Vadich007/shortener/pkg/shorter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetLinkNotExist(t *testing.T) {
+	repo := repository.NewInMemoryLinkRepository()
+	serv := NewLinkService(repo)
+
+	link, err := serv.GetLink("notExist")
+
+	assert.Equal(t, link, "")
+	assert.Equal(t, err.Error(), "link doesn't exist")
+}
+
+func TestGetLinkExist(t *testing.T) {
+	repo := repository.NewInMemoryLinkRepository()
+	serv := NewLinkService(repo)
+	originalName := "link"
+	shortedLink := "short"
+	repo.AddLink(shortedLink, originalName)
+
+	link, err := serv.GetLink(originalName)
+
+	assert.Equal(t, link, originalName)
+	assert.Equal(t, err, nil)
+}
+
+func TestAddLinkExist(t *testing.T) {
+	repo := repository.NewInMemoryLinkRepository()
+	serv := NewLinkService(repo)
+	originalName := "link"
+	expectedShortedLink := "http://localhost:8080/" + shorter.Shorten(originalName)
+	repo.AddLink(shorter.Shorten(originalName), originalName)
+
+	actualShortedLink, err := serv.AddLink(originalName)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, actualShortedLink, expectedShortedLink)
+}
+
+func TestAddLinkNotExist(t *testing.T) {
+	repo := repository.NewInMemoryLinkRepository()
+	serv := NewLinkService(repo)
+	originalName := "link"
+	expectedShortedLink := "http://localhost:8080/" + shorter.Shorten(originalName)
+
+	actualShortedLink, err := serv.AddLink(originalName)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, actualShortedLink, expectedShortedLink)
+}
